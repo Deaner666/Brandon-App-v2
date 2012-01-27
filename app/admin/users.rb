cf1e552100e6f74f39bcaaa0334c469ed9d59fbe
Dave Dean <mail@dave-dean.co.uk>
2012-01-27 16:07:19 +0000

    Hopefully got imports working

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register User do
   index do
     column :email
     column :username
+    column :depot_name
     column :current_sign_in_at
     column :last_sign_in_at
     column :sign_in_count
@@ -15,6 +16,13 @@ ActiveAdmin.register User do
       f.input :username
       f.input :password
       f.input :password_confirmation
+      f.input :depot_number
+      f.input :depot_name
+      f.input :address1
+      f.input :address2
+      f.input :address3
+      f.input :address4
+      f.input :postcode
     end
     f.buttons
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ActiveRecord::Base
   
   validates_presence_of :username
   validates_uniqueness_of :username
-  validates_numericality_of :depot_number, :only_integer => true
+  # validates_numericality_of :depot_number, :only_integer => true
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me, :depot_number,
+                  :depot_name, :address1, :address2, :address4, :address4, :postcode
 end
