5f74e19efd6a225bcc9f6fccbfc4d65e2d65f334
Dave Dean <mail@dave-dean.co.uk>
2012-01-27 12:08:16 +0000

    Added extra fields for username into admin panels

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register User do
   
   index do
     column :email
+    column :username
     column :current_sign_in_at
     column :last_sign_in_at
     column :sign_in_count
@@ -11,6 +12,7 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs "User Details" do
       f.input :email
+      f.input :username
       f.input :password
       f.input :password_confirmation
     end
