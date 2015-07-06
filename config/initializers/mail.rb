ec9802a49ad0d940324318b32940b661d3550fd8
Dave Dean <dave@moortor-design.co.uk>
2015-07-06 21:53:04 +0100

    Updated mailer settings to use server variables.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,8 +3,10 @@ if(Rails.env.production?)
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app1887297@heroku.com'],
-    :password       => ENV['4i2tpoik0407'],
+    # :user_name      => ENV['app1887297@heroku.com'],
+    # :password       => ENV['4i2tpoik0407'],
+    :user_name		=> ENV['SENDGRID_USERNAME'],
+    :password		=> ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com'
   }
   ActionMailer::Base.delivery_method = :smtp
