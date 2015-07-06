8de45c026773512646f9fa0c89c2dd67afc730b2
Dave Dean <dave@moortor-design.co.uk>
2015-07-06 21:49:57 +0100

    Updated heroku domain to herokuapp.com.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ BrandonApp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'brandon-drinks.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'brandon-drinks.herokuapp.com' }
   
   # Enable threaded mode
   # config.threadsafe!
